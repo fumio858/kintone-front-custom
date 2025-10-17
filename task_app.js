9cc827dfc354bcf23fb8124703b32abb4ee2b0b0
廣瀬文雄 <hirose@rainmakers.co.jp>
2025-10-17 17:25:54 +0900

    まとめコミット

--- task_app.js
+++ task_app.js
@@ -192,7 +192,7 @@ const APP_ID_TO_CASE_TYPE = { // NEW
         const div = document.createElement('div');
         div.style.display = 'grid'; div.style.gridTemplateColumns = '1fr auto auto auto';
         div.style.gap = '8px'; div.style.alignItems = 'center'; div.style.padding = '6px 8px';
-        div.style.background = '#F7F9FB'; div.style.borderRadius = '6px';
+        div.style.background = '#EBEFF4'; div.style.borderRadius = '6px';
         div.innerHTML = `
           <div>
             <div style="font-weight:600">${title}</div>
