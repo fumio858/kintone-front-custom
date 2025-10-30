97acdffaf5e88e9189cc8a773b6073b58136bd59
廣瀬文雄 <hirose@rainmakers.co.jp>
2025-10-30 17:14:28 +0900

    まとめコミット

--- calendar_customization.js
+++ calendar_customization.js
@@ -112,6 +112,7 @@
             item.style.padding = '2px 4px'; // 見栄え調整
             item.style.margin = '2px'; // 見栄え調整
             item.style.borderRadius = '3px'; // 見栄え調整
+            item.style.listStyle = 'none'; // 見栄え調整 
           }
 
           // ステータスが「完了」ならクラスを付与
