5600f7429ec98a87883a16703f9b47b59fb33453
廣瀬文雄 <hirose@rainmakers.co.jp>
2025-11-07 13:19:12 +0900

    まとめコミット

--- src/record-priority/detail-custom.js
+++ src/record-priority/detail-custom.js
@@ -116,6 +116,7 @@
         if (labelElement) {
           const title = labelElement.getAttribute('title');
           if (title === '現在の作業者を変更') {
+            el.style.backgroundColor = '#ecf6fb';
             el.style.borderRadius = '8px';
             el.style.border = 'none';
           } else if (title === '案件終了') {
